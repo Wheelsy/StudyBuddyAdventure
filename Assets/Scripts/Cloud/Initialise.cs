339de981059c1a80109de7d2b9aa1b7372ff8676
Wheelsy <elyh117@gmail.com>
2022-07-04 09:24:01 +1000

    tinkering

--- Assets/Scripts/Cloud/Initialise.cs
+++ Assets/Scripts/Cloud/Initialise.cs
@@ -1,10 +1,10 @@
 using Unity.Services.Core;
 using UnityEngine;
 using Unity.Services.Authentication;
-using Unity.Services.CloudSave;
 using System.Threading.Tasks;
 using TMPro;
 using GooglePlayGames;
+using UnityEngine.Android;
 
 public class Initialise : MonoBehaviour
 {
@@ -15,46 +15,10 @@ public class Initialise : MonoBehaviour
     [SerializeField]
     private GameObject loginScreen;
 
-    private async void Awake()
+    async void Awake()
     {
         await UnityServices.InitializeAsync();
-        Debug.Log(UnityServices.State);       
-    }
-
-    private void Start()
-    {
         InitializePlayGamesLogin();
-        SetupEvents();
-    }
-
-    // Setup authentication event handlers if desired
-    void SetupEvents()
-    {
-        AuthenticationService.Instance.SignedIn += () => {
-            // Shows how to get a playerID
-            Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
-
-            // Shows how to get an access token
-            Debug.Log($"Access Token: {AuthenticationService.Instance.AccessToken}");
-        };
-
-        AuthenticationService.Instance.SignInFailed += (err) => {
-            Debug.LogError(err);
-        };
-
-        AuthenticationService.Instance.SignedOut += () => {
-            Debug.Log("Player signed out.");
-        };
-
-        AuthenticationService.Instance.Expired += () =>
-        {
-            Debug.Log("Player session could not be refreshed and expired.");
-        };
-    }
-
-    public void LoginGooglePlayGames()
-    {       
-        Social.localUser.Authenticate(OnGooglePlayGamesLogin);
     }
 
     void InitializePlayGamesLogin()
@@ -62,23 +26,20 @@ public class Initialise : MonoBehaviour
         PlayGamesPlatform.Activate();
     }
 
+    // Call when login button is pressed
+    public void LoginGooglePlayGames()
+    {       
+        Social.localUser.Authenticate(OnGooglePlayGamesLogin);
+    }
+
+    //Check login success
     async void OnGooglePlayGamesLogin(bool success)
     {
         if (success)
         {
             // Call Unity Authentication SDK to sign in or link with Google.
-            // string idToken = ((PlayGamesLocalUser)PlayGamesPlatform.Instance.localUser).GetIdToken();
-            //string idToken = PlayGamesPlatform.Instance.GetServerAuthCode();
             string idToken = PlayGamesPlatform.Instance.GetUserId();
-            Debug.Log("google id: " + idToken);
-            if (idToken == null || idToken.Trim().Equals(""))
-            {
-                Debug.Log("token is null");
-            }
-            else
-            {
-                await SignInWithGoogleAsync(idToken);              
-            }
+            await SignInWithGoogleAsync(idToken);                          
         }
         else
         {
@@ -92,14 +53,13 @@ public class Initialise : MonoBehaviour
         {
             await AuthenticationService.Instance.SignInWithGoogleAsync(idToken);
             Debug.Log("SignIn is successful.");
-            //if sign in successful check for load data and assign player id to in game ui
+            //if sign in successful check for load data and assign player id to in game ui.
+            //Turn login screen off.
             loginScreen.SetActive(false);
-            gameObject.GetComponent<CloudLoad>().CheckForLoadData();
             playerId.text = AuthenticationService.Instance.PlayerId.ToString();
         }
         catch (AuthenticationException ex)
         {
-            Debug.Log("error signing in with unity cloud");
             // Compare error code to AuthenticationErrorCodes
             // Notify the player with the proper error message
             Debug.LogException(ex);
